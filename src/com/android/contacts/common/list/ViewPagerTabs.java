97cb415e0b4b804dffe8a239819ee561a1697866
wangjing <wangjing@codeaurora.org>
2015-04-09 18:08:06 +0800

    ContactsCommon: Tab not change on RTL mode
    Tab view and tab title position are error on RTL mode
    
    Change tab view and title position on RTL mode
    
    Change-Id: Id1d9c92220ca1876dcef08ea64ada819c654c60d
    CRs-Fixed: CR811573

--- src/com/android/contacts/common/list/ViewPagerTabs.java
+++ src/com/android/contacts/common/list/ViewPagerTabs.java
@@ -220,5 +220,9 @@ public class ViewPagerTabs extends HorizontalScrollView implements ViewPager.OnP
         }
         return position;
     }
+
+    public void setTextViewSelected(int position, boolean selected) {
+        mTabStrip.getChildAt(position).setSelected(selected);
+    }
 }
 
